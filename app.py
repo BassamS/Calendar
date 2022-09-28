2453613ae796c249553ee5e57f544feca708654f
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-28 17:25:55 +0200

    Final commit

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from cProfile import label
 from tkinter import *
 import calendar
 
-text = calendar.Calendar(2023)
+text = calendar.calendar(2022)
 root = Tk()
-root.geometry('500x600')
+root.geometry('550x600')
 root.title('CALENDAR')
 label1 = Label(root, text='CALENDAR', bg='dark gray',
                font=('times', 28, 'bold'))
 label1.grid(row=1, column=1)
 root.config(background='white')
-l1 = Label(root, text=text, font='Consoles 10')
+l1 = Label(root, text=text, font='Consolas 10')
+l1.grid(row=2, column=1, padx=20)
+root.mainloop()
